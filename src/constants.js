d4fd7b66ef1a80aed3f75e2e9429bd4f3cb87472
Jyoti Palariya <palariyajyoti456@gmail.com>
2025-10-29 20:55:08 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,285 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/webverse_logo.png';
+import agcLogo from './assets/company_logo/agc_logo.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+// Education Section Logo's
+import glaLogo from './assets/education_logo/gehu.jpg';
+import bsaLogo from './assets/education_logo/bsa_logo.png';
+import vpsLogo from './assets/education_logo/class.png';
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/url.png';
+import csprepLogo from './assets/work_logo/voice.png';
+import movierecLogo from './assets/work_logo/crud.png';
+import taskremLogo from './assets/work_logo/atm.png';
+import npmLogo from './assets/work_logo/currency.png';
+import webverLogo from './assets/work_logo/e commerce.png';
+
+import imagesearchLogo from './assets/work_logo/simulator.png';
+import removebgLogo from './assets/work_logo/leader.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      
+      
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+     
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      { name: 'SQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'Java', logo: javaLogo },
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Python', logo: pythonLogo },
+     
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+     
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+  id: 0,
+  img: webverseLogo,
+  role: "Full Stack Developer",
+  company: "Webverse Digital",
+  date: "April 2025 – Present",
+  desc: "Contributing to the development of responsive and scalable web applications using the MERN stack. Focused on building clean UIs with React and Tailwind, integrating REST APIs, and ensuring smooth backend functionality with Node.js and MongoDB. Collaborating with the team in an agile setup to deliver optimized, user-friendly solutions.",
+  skills: [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React.js",
+    "Node.js",
+    "MongoDB",
+    "Express.js",
+    "Tailwind CSS",
+    "TypeScript",
+    "Next.js",
+  ],
+},
+
+    {
+      id: 1,
+      img: agcLogo,
+      role: "Fullstack Engineer",
+      company: "Agumentik Group of Companies",
+      date: "July 2023 - March 2024",
+      desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+      skills: [
+        "ReactJS",
+        "Redux",
+        "JavaScript",
+        "Tailwind CSS",
+        "HTML",
+        "CSS",
+        "SQL",
+      ],
+    },
+    {
+      id: 2,
+      img: newtonschoolLogo,
+      role: "Frontend Intern",
+      company: "Newton School",
+      date: "September 2021 - August 2022",
+      desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+      skills: [
+        "HTML",
+        "CSS",
+        "Javascript",
+        "Bootstrap",
+        "Figma",
+        "Material UI",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: glaLogo,
+      school: "Graphic Era Hill University, Uttarakhand",
+      date: "Sept 2022 - July 2026",
+      grade: "7.81 CGPA",
+      desc: "I have completed my Bachelor's degree (B.Tech) in Computer Science and Engineering from Graphic Era Hill University, Bhimtal. During my time at Gehu, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at Gehu has been instrumental in shaping my technical abilities and professional growth.",
+      degree: "Bachelor of Technology - B.Tech (Computer Science and Engineering)",
+    },
+   
+    {
+      id: 2,
+      img: vpsLogo,
+      school: "Government Inter Collage , Uttarakhand",
+      date: "Apr 2021 - March 2022",
+      grade: "72%",
+      desc: "I completed my class 12 education from Government Inter Collage, Uttarakhand, under the UK board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+      degree: "UK(XII) - PCM with Computer Science",
+    },
+    {
+      id: 3,
+      img: vpsLogo,
+      school: "Government Inter Collage , Uttarakhand",
+      date: "Apr 2019- March 2020",
+      grade: "80.00%",
+      desc: "I completed my class 10 education from Government Inter Collage , Uttarakhand, under the UK board, where I studied Science with Computer.",
+      degree: "UK(X), Science with Computer Application",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "URL Shortener",
+      description:
+        "A simple web application that takes a long URL as input and returns a shortened URL for easy sharing.",
+      image: githubdetLogo,
+      tags: ["HTML", "CSS", "JavaScript", "API"],
+      github: "https://github.com/jyotipalariya456/URL-shortner",
+      webapp: "https://url-shortner-beta-wine.vercel.app/",
+    },
+    {
+      id: 1,
+      title: "Voice-Vision",
+      description:
+        "Convert your speech or text into images using AI!",
+      image: csprepLogo,
+      tags: ["Python"],
+      github: "https://github.com/jyotipalariya456/voicevision",
+      webapp: "https://github.com/jyotipalariya456/voicevision",
+    },
+    {
+      id: 2,
+      title: "CRUD Application",
+      description:
+        "A CRUD-based web application that allows users to create, read, update, and delete records using PHP and MySQL with a simple, user-friendly interface.",
+      image: movierecLogo,
+      tags: [ "Php","MySQL"],
+      github: "https://github.com/jyotipalariya456/CRUD-Application",
+      webapp: "https://github.com/jyotipalariya456/CRUD-Application",
+    },
+    {
+      id: 3,
+      title: "Currency Converter",
+      description:
+        "A simple and responsive currency converter that provides real-time exchange rate conversions using an external API.",
+      image: npmLogo,
+      tags: [ "Java"],
+      github: "https://github.com/jyotipalariya456/Project_for_codesoft/tree/main/converter",
+      webapp: "https://github.com/jyotipalariya456/Project_for_codesoft/tree/main/converter",
+    },
+    {
+      id: 4,
+      title: "ATM Interface",
+      description:
+        "An ATM interface simulation that allows users to perform basic banking operations like balance inquiry, cash withdrawal, and deposit.",
+      image: taskremLogo,
+      tags: [ "Java"],
+      github: "https://github.com/jyotipalariya456/Project_for_codesoft/tree/main/atmInterface",
+      webapp: "https://github.com/jyotipalariya456/Project_for_codesoft/tree/main/atmInterface",
+    },
+    {
+      id: 5,
+      title: "E-Commerce Website",
+      description:
+        "An Amazon-inspired e-commerce website featuring product listings, user authentication, and cart management.",
+      image: webverLogo,
+      tags: [ "JavaScript", "HTML", "CSS"],
+      github: "https://github.com/jyotipalariya456/E-commerce-Website",
+      webapp: "https://github.com/jyotipalariya456/E-commerce-Website",
+    },
+    
+    {
+      id: 7,
+      title: "Page Replacement Algorithms Simulation",
+      description:
+        "The goal of the Page Replacement Algorithm Simulator project was to provide a platform to simulate and compare various page replacement algorithms like FIFO, LRU, MRU, OPR. ",
+      image: imagesearchLogo,
+      tags:  ["JavaScript", "Html", "Css","Firebase","Material-UI"],
+      github: "https://github.com/jyotipalariya456/page-simulator",
+      webapp: "https://github.com/jyotipalariya456/page-simulator",
+    },
+    {
+      id: 8,
+      title: "Leader-Board App",
+      description:
+        "A dynamic leaderboard system where users can log in, claim points, and view real-time rankings",
+      image: removebgLogo,
+      tags: ["HTML", "CSS", "Javascript"],
+      github: "https://github.com/jyotipalariya456/LeaderBoard",
+      webapp: "https://app-leader-board.netlify.app/",
+    },
+  ];  
