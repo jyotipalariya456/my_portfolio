d4753e2205617a3639eea893d52cfcd4d08eb679
Jyoti Palariya <palariyajyoti456@gmail.com>
2025-11-01 21:55:29 +0530

    fix contacts

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -12,10 +12,10 @@ const Contact = () => {
 
     emailjs
       .sendForm(
-        "service_axbtt7a",  // Replace with your EmailJS Service ID
-        "template_1ziboq3",  // Replace with your EmailJS Template ID
+        "service_fux4ggb",  // Replace with your EmailJS Service ID
+        "template_xjnjfza",  // Replace with your EmailJS Template ID
         form.current,
-        "Rz7W9pVF0HdDryNNL"  // Replace with your EmailJS Public Key
+        "BeHCfvhZja7ByNwbR"  // Replace with your EmailJS Public Key
       )
       .then(
         () => {
